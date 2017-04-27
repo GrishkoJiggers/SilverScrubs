c80d8fd42bf74f91ffb0ee0a3f03fe42d5e5ef03
GrishkoJiggers <derangedpigeon@gmail.com>
2017-04-27 12:25:14 -0700

    Update world.py

--- world.py
+++ world.py
@@ -10,14 +10,36 @@ from encounter import *
 
          
 
-def checkInput(inputprompt, allowed, errormsg):
+def checkInput(prompt, type_=None, min_=None, max_=None, range_=None):
+    if min_ is not None and max_ is not None and max_ < min_:
+        raise ValueError("min_ must be less than or equal to max_.")
     while True:
-        input_ = int(input(str(inputprompt)))
-        if input_ in allowed:
-            return input_
-            break
+        ui = input(prompt)
+        if type_ is not None:
+            try:
+                ui = type_(ui)
+            except ValueError:
+                print("Input type must be {0}.".format(type_.__name__))
+                continue
+        if max_ is not None and ui > max_:
+            print("Input must be less than or equal to {0}.".format(max_))
+        elif min_ is not None and ui < min_:
+            print("Input must be greater than or equal to {0}.".format(min_))
+        elif range_ is not None and ui not in range_:
+            if isinstance(range_, range):
+                template = "Input must be between {0.start} and {0.stop}."
+                print(template.format(range_))
+            else:
+                template = "Input must be {0}."
+                if len(range_) == 1:
+                    print(template.format(*range_))
+                else:
+                    print(template.format(" or ".join((", ".join(map(str,
+                                                                     range_[:-1])),
+                                                       str(range_[-1])))))
         else:
-            print(str(errormsg)+"\n")
+            return ui
+            #print(str(errormsg)+"\n")
 
 # This function is what I use throughout the program to check whether an input falls within a set of expected values.
 # If an invalid input is given, a message specific to the context is returned.
@@ -27,17 +49,17 @@ def createChar():
     print("Background: "+"\n")
     for item in starts:
         print(str(starts.index(item))+": "+item[0]+": ")
-        print(item[7])
+        print(item[6])
         print("************************************************************************************************")
-        print("     Strength: "+str(item[2])+".")
-        print("     Agility: "+str(item[3])+".")
-        print("     Intelligence: "+str(item[4])+".")
-        print("     Luck: "+str(item[5])+".")
-        print("     Tenacity: "+str(item[6])+"."+"\n")
+        print("     Strength: "+str(item[1])+".")
+        print("     Agility: "+str(item[2])+".")
+        print("     Intelligence: "+str(item[3])+".")
+        print("     Luck: "+str(item[4])+".")
+        print("     Tenacity: "+str(item[5])+"."+"\n")
     global character, startDesc
-    character = checkInput("Choose a background: "+"\n", list(range(0, len(starts))), "Invalid background; choose a number corresponding to a background. "+"\n")
-    player = Player(w, starts[character][1], starts[character][2], starts[character][3], starts[character][4], starts[character][5], starts[character][6])
-    startDesc = starts[character][8]
+    character = checkInput("Choose a background: "+"\n", type_=int, min_=0, max_=len(starts)-1)
+    player = Player(w, starts[character][1], starts[character][2], starts[character][3], starts[character][4], starts[character][5], starts[character][8])
+    startDesc = starts[character][7]
     return player
 
 class World:
@@ -50,40 +72,8 @@ class World:
         self.pdamagedealt = False
         self.edamagedealt = False
         self.currenemy = None
-        self.generated = [[1,5, True]]
+        self.day = 1
        
-        # Above is a list of coordinates that have already been generated.
-        # Locations are stored as lists of [row, column, event]. If the event at an area has been executed, the value stored in the last position will be defined by the encounter. If 
-        # it's already been encountered, the "encountered" attribute is True. In the event the player visists a cell with the "encountered" attribute, something like "The house you
-        # saw before is still here," or something like that. Otherwise, it will be treated as a new encounter. The starting cell is an exception, that being the default True value for encountered. If the player moves to a cell with cell[2] == "Start", a specific message will be displayed regarding the current region. The last (fourth) element of the list
-        # will be the name of the encounter variable, which will be removed from the list of possible encounters to eliminate repetitiveness. 
-        #  
-        # The player starts at [1,5], so this set of coordinates is already in the list. This feature is meant to avoid coming back to an area only to see it's become something else entirely-- 
-        # once loaded, it stays that way.
-
-        # The map works like this: 
-        # REGION: Fields, Forests, Mountains, Homefront
-        # 0 : 0 1 2 3 4 5 6 7 8 9 10       N
-        # 1 : 0 1 2 3 4 5 6 7 8 9 10   W --|-- E
-        # 2 : 0 1 2 3 4 5 6 7 8 9 10       S
-        # 3 : 0 1 2 3 4 5 6 7 8 9 10
-        # 4 : 0 1 2 3 4 5 6 7 8 9 10
-        # 5 : 0 1 2 3 4 5 6 7 8 9 10
-        # 6 : 0 1 2 3 4 5 6 7 8 9 10
-        # 7 : 0 1 2 3 4 5 6 7 8 9 10
-        # 8 : 0 1 2 3 4 5 6 7 8 9 10
-        # 9 : 0 1 2 3 4 5 6 7 8 9 10
-
-        # The player needs to move to the tenth row to the south to progress to the next region. It takes one day to move to a new location. If the player lingers for more than 15 days, 
-        # an extremely powerful enemy will catch up. At each location, the possible events for the W, SW, S, SE, and S directions will be pre-generated.
-
-    #def loadNext(self):
-
-    #def encounterLoad(self):
-    #    loadlist = []
-    #    for coords in [[self.p.coords[0]-1, self.p.coords[1], False], [self.p.coords[0]-1, self.p.coords[1]-1, False],[self.p.coords[0], self.p.coords[1]-1, False],[self.p.coords[0]+1, self.p.coords[1]-1, False],[self.p.coords[0]+1, self.p.coords[1], False],[self.p.coords[0]+1, self.p.coords[1]+1, False],[self.p.coords[0], self.p.coords[1]+1, False],[self.p.coords[0]-1, self.p.coords[1]+1], False] 
-    #        self.generated.append(coords)
-
     def update(self, enemy):
         if self.p.abilities[self.p.ability][0] == "Observe":
             if self.p.inc >= 8:
@@ -150,7 +140,7 @@ class World:
                     enemy.health -= enemy.limbs[self.p.targetlimb][2]*self.p.damage
                     enemy.health = int(enemy.health)
                 else:
-                    print(str(enemy.name)+" failed to dodge your "+str(self.p.abilities[self.p.ability][0])+" and took "+str(self.p.damage)+" damage to the"+enemy.limbs[self.p.targetlimb][0]+"."+"\n")
+                    print(str(enemy.name)+" failed to dodge your "+str(self.p.abilities[self.p.ability][0])+" and took "+str(self.p.damage)+" damage to the "+enemy.limbs[self.p.targetlimb][0]+"."+"\n")
                     enemy.limbs[self.p.targetlimb][1] -= self.p.damage
                     enemy.health -= enemy.limbs[self.p.targetlimb][2]*self.p.damage
                     enemy.health = int(enemy.health)
@@ -222,7 +212,7 @@ class World:
                 print(str(entity.name)+" "+limb[0]+" crippled."+"\n")
             elif limb[1]<int((entity.ten/10)*200*0.66):
                 print(str(entity.name)+" "+limb[0]+" weakened."+"\n")
-
+            
 
     def initiate(self, enemy):
         player = self.p
@@ -247,6 +237,7 @@ class World:
             
             if enemy.health <= 0:
                 print(str(enemy.name)+" has died."+"\n")
+                enemy.loot()
                 break
             if self.p.health <= 0:
                 print("You have died."+"\n")
@@ -264,29 +255,23 @@ class World:
         print("Fight over.")
         self.currenemy = None
 
-######################################################################################################################################################################################################
-
-
-
-
-############################################################################################################################################################################################################
-
-
-
-
-# Note: have weapon attacks be elements of the weapon (stab, slash, overhead) as dictionaries with the keys as attack names and 
-# values as turn costs.
-# Player abilities (headbutt, throw, etc...) are elements of the player instead
-
 
 w = World()
 
-longsword = Sword(w,"Longsword", 8, 17)
-shortsword = Sword(w, "Shortsword", 7, 13)
-broadsword = Sword(w,"Broadsword", 9, 16)
-battleaxe = Axe(w,"Battleaxe", 8, 17)
-hoe = Axe(w,"Rusty hoe", 5, 10)
-starts = [["SCOUT", shortsword, 3, 6, 6, 7, 4, "Quick and precise, with relatively high critical chance and perception. Starts with a shortsword.", "You are a scout of the Slate Empire. Time and time again, you have watched as the fertile fields and lush forests you scouted were mired with the blood of enemies and allies alike. This time, however, you decide that this war-- your war, is over."], ["INFANTRYMAN", broadsword, 6, 5, 4, 5, 6, "Common foot soldier with generally well-rounded stats. Starts with a broadsword.", "Freshly drafted from a small farm in the South, you have fought on the front lines enough to be considered a miraculous survivor, as not many survive the front lines. Every battle may be one's last-- and this is not where you die."], ["OFFICER", longsword, 4, 4, 8, 4, 5, "Low-ranking officer. Not particularly skilled, but relatively clever and well-equipped. Starts with a longsword, a dagger, and armor.", "Recruited from a family of poor nobles, you have been hastily trained to command troops on the battlefield, despite having no experience in combat. It did not take long, however, for your appetite for war to be sated-- shortly after your first foray into the Ashen Wood, you have seen enough. You decide that you'd rather live in shame than die like so many of your subordinates."]]
+longsword = Sword(w,"Longsword", "A large double-edged sword. Heavy, but reliable damage.", 8, 17)
+shortsword = Sword(w, "Shortsword", "A standard military shortsword. Usually not a first choice for a weapon, but in a pinch, it can prove useful.", 7, 13)
+broadsword = Sword(w,"Broadsword", "The most common weapon on the battlefield, with a wide blade. Short and sturdy.",  9, 16)
+battleaxe = Axe(w,"Battleaxe", "A heavy and somewhat dull axe, favored by bandits and soldiers alike.", 7, 18)
+hoe = Axe(w,"Rusty hoe", "Definitely not meant to be used as a weapon-- still, it is hard and heavy; a weapon for the desperate.", 5, 10)
+onehit = Sword(w, "Onehitter", "A magical item used only by the person testing the code.", 200, 202)
+
+salve = Consumable(w, "Healing salve", "A medicinal salve made by soaking medicinal ointments and herbs into a bandage. Heals for 30 HP.", "Healing", heal=30)
+luckrock = Consumable(w, "Lucky rock", "Folk tales tell that tossing this rock as far as you can brings good luck. It couldn't hurt to try...", "Stat", stat="Luck", statnum=2)
+serum = Consumable(w, "Serum", "Mystics and druids often peddle snake oils claiming to bring supernatural strength. Though unlikely, these substances were banned in many athletic competitions back home... so perhaps they work after all?", "Stat", stat="Strength", statnum=1)
+
+
+
+starts = [["SCOUT", 3, 6, 6, 7, 4, "Quick and precise, with relatively high critical chance and perception. Starts with a shortsword.", "You are a scout of the Slate Empire. Time and time again, you have watched as the fertile fields and lush forests you scouted were mired with the blood of enemies and allies alike. This time, however, you decide that this war-- your war, is over.", {shortsword.itemname:shortsword, salve.itemname:salve}], ["INFANTRYMAN", 6, 5, 4, 5, 6, "Common foot soldier with generally well-rounded stats. Starts with a battleaxe.", "Freshly drafted from a small farm in the South, you have fought on the front lines enough to be considered a miraculous survivor, as not many survive the front lines. Every battle may be one's last-- and this is not where you die.", {battleaxe.itemname:battleaxe, salve.itemname:salve}], ["OFFICER", 4, 4, 8, 4, 5, "Low-ranking officer. Not particularly skilled, but relatively clever and well-equipped. Starts with a longsword, a dagger, and armor.", "Recruited from a family of poor nobles, you have been hastily trained to command troops on the battlefield, despite having no experience in combat. It did not take long, however, for your appetite for war to be sated-- shortly after your first foray into the Ashen Wood, you have seen enough. You decide that you'd rather live in shame than die like so many of your subordinates.", {longsword.itemname:longsword, salve.itemname:salve, luckrock.itemname:luckrock}]]
 
 w.p = createChar()
 
@@ -295,7 +280,6 @@ w.p = createChar()
 peasant = Human(w, hoe, "Peasant", 3, 3, 1, 1, 1, "It looks like a peasant. He's angry.", "An angry peasant. Basically as weak as they come, in most respects."+"\n")
 bandit = Human(w, battleaxe, "Bandit", 7, 3, 1, 5, 7, "A big, scary bandit. He looks angry.", "A bandit common to these parts. Strong, but not too quick."+"\n")
 thief = Human(w, broadsword, "Thief", 5, 10, 4, 10, 4, "He looks skinny. Probably a thief.", "A thief looking to make a score. He looks quick, and probably relies heavily on luck for damage."+"\n")
-day = 1
 
 startspiel = ("The Republic of Ashen Wood and Empire of Slate, the two great powers of the world, have been at war for centuries. The reason for the war has been lost to time, as the conflict is as old as the countries that wage it. The last few decades of the Slate Empire have been marked by a ruthless campaign north towards the heart of Ashen Wood-- at devastating costs to both sides."+"\n")
 
@@ -305,17 +289,7 @@ testencounter = Encounter(w, "This is a test encounter.", "Do nothing", option2 
 testencounter1 = Encounter(w, "You stumble upon a cottage by a creek. It seems as though someone managed to remain undetected by the armies of Slate. Smoke rises from the chimney, and there is light inside the cottage." , "Knock on the door", option1 = "Smash down the door.", option2="Leave.", enemy = thief, outcome = ["The door is unlocked. As you enter, you see a thief rifling through the remains of a man and woman, blood fresh on their clothes. Seeking to protect his loot, the thief prepares to make another score.", 0], outcome1 = ["A merchant is tied up inside the house. You untie him, and as thanks, he offers to show you his wares.", 2], outcome2 = ["You prepare to leave.", 3])
 testencounter2 = Encounter(w, "You meet a strange old man.", "Talk to him", enemy = peasant, option1 = "Threaten him to give you his valuables.", outcome = ["'Hello,' says the old man.",1], outcome1 = ["Oh, no you don't, sonny Jim!",0], prompt = "Tell me, what bread is best bread?", conv = "Garlic bread is best bread.", resp = ["'That is correct,' he says. You shrug and prepare to move on.", 3], conv1 = "I dunno... French bread?", resp1 = ["'That is incorrect! Prepare for destruction!' he shrieks. Looks like you have to put the old man down.",0])
 encounters1 = [testencounter, testencounter1, testencounter2]
-while True:
-    if day == 1:
 
-        startencounter.runEncounter()
-        day +=1
-    elif day == 2:
-        testencounter2.runEncounter()
-        day += 1
-    elif day == 3:
-        testencounter1.runEncounter()
-        break
         
 
 
